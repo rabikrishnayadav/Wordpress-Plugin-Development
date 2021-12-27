6df4d1a419a9d26c243a3d76e2a3ab8f428eb0e7
Rabi Kr Yadav <rabikrishnayadav@gmail.com>
2021-12-27 22:28:31 +0545

    main file created

---
+++ wp_plugin_development.php
@@ -0,0 +1,12 @@
+<?php
+/**
+
+*Plugin Name: Wp Plugin Development
+*Plugin Uri: https://vidsik.com.np
+*Author: Rabi Kr Yadav
+*Author Uri: https://rabikrishnayadav.com.np
+*Description: With This plugin i am going to learn how to make wordpress plugin
+*Version: 1.0.0
+*Lisence: GPL V2
+
+*/
