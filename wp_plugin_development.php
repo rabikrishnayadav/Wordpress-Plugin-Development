eae588bce6857bf332db758bf96e818dad02b758
Rabi Kr Yadav <rabikrishnayadav@gmail.com>
2022-01-25 13:25:07 +0545

    Menu Added

--- wp_plugin_development.php
+++ wp_plugin_development.php
@@ -27,3 +27,30 @@ function vswp_enqueue_scripts(){
 
 	wp_enqueue_script('vswp_plugin_js', plugin_dir_url(__FILE__).'assets/js/custom.js',array(),'1.0.0',true); // for add jss file
 }
+
+
+// add top-level Menu or Sub-Menu om Menu Bar
+
+add_action('admin_menu','vswp_plugin_menu');
+
+function vswp_plugin_menu(){
+
+	add_menu_page('VSWP Option','VSWP Option','manage_options','vswp-options','vswp_options_func', $icon_url = '', $position = null);
+
+	add_submenu_page('vswp-options','VSWP Layout','VSWP Layout','manage_options','vswp-settings','vswp_layout_func');
+
+	add_submenu_page('vswp-options','VSWP Setting','VSWP Setting','manage_options','vswp-layout','vswp_settings_func');
+}
+
+function vswp_options_func(){
+
+	echo "<h1 style='text-align:center'>VSWP Options Menu</h1>";
+}
+function vswp_layout_func(){
+
+	echo "<h1 style='text-align:center'>VSWP Layout Menu</h1>";
+}
+function vswp_settings_func(){
+
+	echo "<h1 style='text-align:center'>VSWP Settings Menu</h1>";
+}
