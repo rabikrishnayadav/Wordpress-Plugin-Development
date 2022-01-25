293dd14639042c39bf7f0664b4188dc3acf2183a
Rabi Kr Yadav <rabikrishnayadav@gmail.com>
2022-01-25 22:23:08 +0545

    post request sent using http api method

--- inc/api.php
+++ inc/api.php
@@ -1,25 +1,39 @@
 <?php 
 if(!defined('ABSPATH')) exit; // Exit if accessed directly.
-$info = wp_remote_retrieve_body(wp_remote_get('https://api.github.com/users/imranrbx'));
-$user = json_decode($info);
+
+$post_info = wp_remote_retrieve_body(wp_remote_post('https://jsonplaceholder.typeicode.com/posts',[
+
+		'body' => [
+
+			'title' => 'This is Dummy Post Title',
+			'body'  => 'This is a body for Dummy post',
+			'userId'=> 10
+		],
+	])
+);
+	
+$posts = wp_remote_retrieve_body(wp_remote_get('https://jsonplaceholder.typeicode.com/posts'));
+$posts = json_decode($post_info);
+
 ?>
+
 <table class="widefat fixed striped">
 	<thead>
 		<tr>
 			<th>ID</th>
-			<th>Image</th>
-			<th>Name</th>
-			<th>Company</th>
-			<th>Location</th>
+			<th>userId</th>
+			<th>Title</th>
+			<th>Body</th>
 		</tr>
 	</thead>
 	<tbody>
+		<?php foreach($posts as $info_data): ?>
 		<tr>
-			<td><?php echo $user->id; ?></td>
-			<td><img src="<?php echo $user->avatar_url; ?>" width="100%"></td>
-			<td><?php echo $user->name; ?></td>
-			<td><?php echo $user->company; ?></td>
-			<td><?php echo $user->location; ?></td>
+			<td><?php echo $info_data->id; ?></td>
+			<td><?php echo $info_data->userId; ?></td>
+			<td><?php echo $info_data->title; ?></td>
+			<td><?php echo $info_data->body; ?></td>
 		</tr>
+		<?php endforeach; ?>
 	</tbody>
 </table>
