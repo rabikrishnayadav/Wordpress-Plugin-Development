1f3b0e28d644abf4d6e4be70e94d9d9265ef5acd
Rabi Kr Yadav <rabikrishnayadav@gmail.com>
2022-01-26 13:16:49 +0545

    external stylesheet add on gutenberg block

--- assets/js/blocks.js
+++ assets/js/blocks.js
@@ -2,31 +2,18 @@
 
 (function(blocks, element){
 	var el = element.createElement;
-	var admin_style = {
-
-		backgroundColor: 'green',
-		padding: '20px',
-		color: 'black'
-	}
-
-	var frontend_style = {
-
-		backgroundColor: 'gold',
-		padding: '20px',
-		color: 'black'
-	}
 	blocks.registerBlockType('vswp-plugin-dev/vswp-block01', {
 
 		title: 'VSWP Block',
 		icon: 'admin-home',
 		category: 'layout',
-		edit: function(){
+		edit: function(prop){
 
-			return el('p',{style: admin_style},"Hello I'm in Editor")
+			return el('p',{className: prop.className},"Hello I'm in Editor")
 		},
-		save: function(){
+		save: function(prop){
 
-			return el('p',{style: frontend_style},"Hello I'm in Frontend")
+			return el('p',{className: prop.className},"Hello I'm in Frontend")
 		}
 	});
 }(window.wp.blocks, window.wp.element));
--- inc/blocks.php
+++ inc/blocks.php
@@ -11,8 +11,16 @@ class vswpBlocks{
 
 		wp_register_script('vswp-script-blocks', plugin_dir_url(__FILE__)."../assets/js/blocks.js", array('wp-blocks','wp-element'));
 
+		wp_register_style('vswp-admin-style', plugin_dir_url(__FILE__)."../assets/css/editor.css");
+
+		wp_register_style('vswp-front-style', plugin_dir_url(__FILE__)."../assets/css/blocks.css");
+
 		$name = "vswp-plugin-dev/vswp-block01";
-		$args = array('editor_script' => 'vswp-script-blocks');
+		$args = array(
+			'style' => 'vswp-front-style',
+			'editor_style' => 'vswp-admin-style',
+			'editor_script' => 'vswp-script-blocks'
+		);
 		register_block_type($name, $args);
 	}
 } new vswpBlocks;
